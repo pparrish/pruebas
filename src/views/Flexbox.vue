f7753374283d6ff9239e43fd37d4ab5e4a3dd6a1
Parrish <paarriisshh@yahoo.com.mx>
2018-06-01 20:58:22 -0500

    Contenido justificado

--- src/views/Flexbox.vue
+++ src/views/Flexbox.vue
@@ -23,9 +23,19 @@
     border: 5px solid #ffcc5c;
     /*Con esto podemos comenzar a crear una caja flex*/
     display: flex;
-    /*flex direction y row y column puede cambiar la orientacion de los objetos dentro*/
-    /*flex-direction: row;*/
-    flex-direction: column;
+    /*flex direction y row y column puede cambiar la orientacion de los objetos dentro, row es por default*/
+    flex-direction: row;
+    /*flex-direction: column;*/
+    /*Justify content puede hacer varial la posicion en la que aparecen los elementos en un principio, para posicionarlos donde nos guste, por defecto flex-start*/
+    /*justify-content: flex-start;*/
+    /*justify-content: flex-end;*/
+    /*justify-content: center;*/
+    /*Espacio exacto entre los elementos, los laterales quedan con menos espacio*/
+    /*justify-content: space-around;*/
+    /*llena los espacios en el centro y no deja espacio a los lados*/
+    /*justify-content: space-between;*/
+    /*Espacio exacto en todo el espacio que no llenan los elementos*/
+    /*justify-content: space-evenly;*/
   }
   #main nav div {
     background: blue;
