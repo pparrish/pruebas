3215b94fd71d207d95f8cd65ffb86fee5408f1ec
Parrish <paarriisshh@yahoo.com.mx>
2018-08-01 21:59:54 -0500

    Add a product name and a description for completing the code challenge

--- src/views/VueCourse.vue
+++ src/views/VueCourse.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h1>Hello</h1>
+        <h1>{{product}}</h1>
+        <p>{{description}}</p>
     </div>
 </template>
 
@@ -9,7 +10,8 @@ export default {
     name:"vue-course",
     data: function() {
         return {
-
+            product: "The incredible shoes",
+            description: "This incredible shoes make you look glamorous and add the capacity to fly, only for 30ns."                 
         }
     }
 }
