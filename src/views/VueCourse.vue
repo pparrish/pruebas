01f35e194eceb56cc6f014d83861cbc80cc01e74
Parrish <paarriisshh@yahoo.com.mx>
2018-08-02 18:44:23 -0500

    Add a "on sale" section

--- src/views/VueCourse.vue
+++ src/views/VueCourse.vue
@@ -10,7 +10,13 @@
             <a :name="top" />
             <h1>{{product}}</h1>
             <p>{{description}}</p>
-            <a :href=" '#' + top">Top</a>
+            <div v-if="onSale">
+                <h6>On sale!!!</h6>
+                <p v-if="higthStock" >In Stock</p>
+                <p v-else-if="almostStock">Run, low stock!!</p>
+                <p v-else>Out of stock</p>
+                <a :href=" '#' + top">Top</a>
+            </div>
         </div>
     </div>
 </div>
@@ -25,8 +31,18 @@ export default {
             product: "The incredible shoes",
             description: "This incredible shoes make you look glamorous and add the capacity to fly, only for 30ns.",                 
             image: shoesImage,
-            top:"top"
-}
+            top: "top",
+            stock: 10,
+            onSale: true
+        }
+    },
+    computed: {
+        higthStock: function(){
+            return this.stock > 10
+        },
+        almostStock: function () {
+            return this.stock <= 10 && this.stock > 0
+        }
     }
 }
 </script>
